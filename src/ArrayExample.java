40c0100840e0100585512446761eb6cd5ef6db30
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-03-20 02:24:51 +0530

    added some array fn

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -1,7 +1,5 @@
 //Full Java program to demonstate array declaration and initailization
 
-
-
 public class ArrayExample {
     public static void main(String[] args) {
 
@@ -11,7 +9,7 @@ public class ArrayExample {
         num[1]=20;
         num[2]=30;
         num[3]=40;
-        num[4]=50;
+        num[4]=15;
         
         System.out.println("Array initalized step by step");
         for (int i=0;i<num.length;i++) {
@@ -24,6 +22,58 @@ public class ArrayExample {
         for (int i=0; i<num2.length; i++) {
             System.out.println(num2[i]);
             }
+        
+//insert a new element at the end
+        int[] newArray=new int[num.length+1];
+        for(int i=0;i<num.length;i++) {
+            newArray[i]=num[i];
+        }
+        newArray[num.length]=60;
+        num=newArray;
+        System.out.println("Array after adding new element");
+        for (int i=0; i<num.length; i++) {
+            System.out.println(num[i]);
+        }
+        System.out.println(num.length);
+        System.out.println(num2.length);
+
+//Delete and element @ index 2
+        int[] updatedArray=new int[num.length-1];
+        for (int i=0;i<num.length-1;i++) {
+            if (i==2)
+                continue;
+            updatedArray[i]=num[i];
+        }
+        num=updatedArray;
+        System.out.println("Update Array: "+java.util.Arrays.toString(num));
+        
+//Searching an element target=30
+        int target=30;
+        boolean found=false;
+        for (int numb : num){
+        if (numb==target){
+            found=true;
+            break;
+        }
+    }
+        System.out.println("Target found: "+found);
+
+//Sorting an Array
+        for (int i=0;i<num.length-1;i++){  
+            for (int j=i+1;j<num.length;j++){
+                if (num[i]>num[j]){
+                    int temp=num[i];
+                    num[i]=num[j];
+                    num[j]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted Array: "+java.util.Arrays.toString(num));
+
+
+
+
+
     }
     
 }
